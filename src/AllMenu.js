01304468396041922154a006ba04a1d33785356a
jensidankhara <jensidankhara2000@gmail.com>
2025-07-02 13:32:49 -0400

    initial commit

---
+++ src/AllMenu.js
@@ -0,0 +1,20 @@
+function AllMenu() {
+    return (
+        <div className="main_mennu_secction">
+            <ul>
+                <li>New</li>
+                <li>Brands</li>
+                <li>Skin Care</li>
+                <li>Hair</li>
+                <li>Fangrace</li>
+                <li>Tool & Brushes</li>
+                <li>Bath & Body</li>
+                <li>Mini Size</li>
+                <li>Beauty Under $20</li>
+                <li>Gift & gift card</li>
+                <li>Sale & Offer </li>
+            </ul>
+        </div>
+    )
+}
+export default AllMenu;
---
+++ src/ProductFilter.js
@@ -0,0 +1,29 @@
+function ProductFilter() {
+    return (
+        <div class="main_product_filter">
+            <div className="">
+                <h1>
+                    Sale
+                </h1>
+                <ul className="all_list_section">
+                    <li>Makeup (312)</li>
+                    <li>Clean at Sephora (247)</li>
+                    <li>Mini Size (104)</li>
+                    <li>Gifts (103)</li>
+                    <li>Skincare (240)</li>
+                    <li>Luxury Beauty (35)</li>
+                    <li>Hair (340)</li>
+                    <li>Tools & Brushes (39)</li>
+                    <li>Men (34)</li>
+                    <li>Bath & Body (39)</li>
+                    <li>Fragrance (125)</li>
+                    
+                </ul>
+
+
+                <path d="M1.5 24a.5.5 0 01-.5-.5v-20a.5.5 0 01.5-.5h21a.5.5 0 01.5.5v20a.5.5 0 01-.5.5h-21zM22 23V4H2v19h20z"></path>
+            </div>
+        </div>
+    )
+}
+export default ProductFilter;
